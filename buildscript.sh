94da3c018a7a38ca9e73addf72b7833c2b96e296
Suresh Kumar Thayumanavan <sureshthayudev@gmail.com>
2020-03-20 07:41:20 +0530

    added build increment.

--- buildscript.sh
+++ buildscript.sh
@@ -10,7 +10,24 @@ mkdir -p "${BUILD_DIR}"
 # compile project
 echo Building Project
 
+echo Increment Build No
+
+PLIST="${PROJECT_NAME}/Info.plist"
+PLB="/usr/libexec/PlistBuddy"
+LAST_NUMBER=$($PLB -c "Print CFBundleVersion" "$PLIST")
+NEW_VERSION=$((LAST_NUMBER + 1))
+echo $NEW_VERSION
+$PLB -c "Set :CFBundleVersion $NEW_VERSION" "$PLIST"
+
 xcodebuild -workspace "${PROJECT_NAME}".xcworkspace -scheme "${PROJECT_NAME}"  archive -configuration release -sdk iphoneos -archivePath "${BUILD_DIR}/${PROJECT_NAME}".xcarchive
+
+echo tag Build No
+
+SHORT_VERSION=$($PLB -c "Print CFBundleShortVersionString" "$PLIST")
+BUILD_NUMBER=$($PLB -c "Print CFBundleVersion" "$PLIST")
+git tag v$SHORT_VERSION-build$BUILD_NUMBER
+#git commit
+#git push
 
 #Check if build succeeded
 if [ $? != 0 ]
@@ -26,4 +43,4 @@ xcodebuild -exportArchive -archivePath  "${BUILD_DIR}/${PROJECT_NAME}".xcarchive
 
 echo Pushing to FB App distribution
 
-firebase appdistribution:distribute "${BUILD_DIR}/${PROJECT_NAME}".ipa --app "FB APP ID" --groups test_group #--token "FB_TOKEN_REFREESH"
+firebase appdistribution:distribute "${BUILD_DIR}/${PROJECT_NAME}".ipa --app "FB_APP_ID" --groups test_group #--token "FB_TOKEN_REFREESH"
